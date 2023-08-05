4c7e2edecc54ac4614e50840323ad56e2537bb5c
Patrick Ducusin <patrickducusin2@gmail.com>
2023-08-05 18:27:43 -0400

    Continue working on making sign-in button in sign-in page

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { FC } from "react";
+
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/Button";
+
+// url = /sign-in, (auth) folder is only for organizational purposes
+const Page: FC = () => {
+  return (
+    <div className="absolute inset-0">
+      <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
+        <Link
+          href="/"
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            "self-start -mt-20"
+          )}
+        >
+          Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
